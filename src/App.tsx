94ae09ebcfaec57881a5146087bea2a0b2101307
nking0715 <talentedblu@gmail.com>
2024-10-14 11:07:48 -0400

    refactor(App): add prettier-lint vscode setting and update types

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import './App.css';
 
 const App = () => {
   const [turn, setTurn] = useState<boolean>(false);
-  const [winner, setWinner] = useState<string | null>(null);
+  const [winner, setWinner] = useState<'Cross' | 'Circle' | 'Tie' | ''>('');
   const [crossArray, setCrossArray] = useState<number[]>([]);
   const [circleArray, setCircleArray] = useState<number[]>([]);
 
   const items: number[] = [11, 12, 13, 21, 122, 23, 31, 32, 33];
-  const winArray: number[] = [36, 63, 69, 96, 166];
+  const WIN_SUMS: number[] = [36, 63, 69, 96, 166];
 
-  const isWin: (array: number[]) => boolean = (array) => {
-    for (let i: number = 0; i < array.length - 1; i++) {
-      for (let j: number = i + 1; j < array.length - 1; j++) {
-        let sum: number = array[i] + array[j] + array[array.length - 1];
-        if (winArray.includes(sum)) return true;
+  const checkWin = (array: number[]): boolean => {
+    for (let i = 0; i < array.length - 1; i++) {
+      for (let j = i + 1; j < array.length - 1; j++) {
+        let sum = array[i] + array[j] + array[array.length - 1];
+        if (WIN_SUMS.includes(sum)) return true;
       }
     }
     return false;
@@ -24,35 +24,35 @@ const App = () => {
     setTurn(Math.random() > 0.5);
     setCrossArray([]);
     setCircleArray([]);
-    setWinner(null);
+    setWinner('');
   };
 
-  const handleTick: (item: number) => void = (item) => {
+  const handleTick = (item: number): void => {
     if (circleArray.includes(item) || crossArray.includes(item)) return;
     if (turn) {
       const newCircleArray = [...circleArray, item];
       setCircleArray(newCircleArray);
-      if (newCircleArray.length + crossArray.length === 9 && !isWin(newCircleArray))
+      if (newCircleArray.length + crossArray.length === 9 && !checkWin(newCircleArray))
         setWinner('Tie');
-      if (newCircleArray.length > 2 && isWin(newCircleArray)) setWinner('Circle');
+      if (newCircleArray.length > 2 && checkWin(newCircleArray)) setWinner('Circle');
     } else {
       const newCrossArray = [...crossArray, item];
       setCrossArray(newCrossArray);
-      if (newCrossArray.length + circleArray.length === 9 && !isWin(newCrossArray))
+      if (newCrossArray.length + circleArray.length === 9 && !checkWin(newCrossArray))
         setWinner('Tie');
-      if (newCrossArray.length > 2 && isWin(newCrossArray)) setWinner('Cross');
+      if (newCrossArray.length > 2 && checkWin(newCrossArray)) setWinner('Cross');
     }
     setTurn(!turn);
   };
 
-  const winnerCheck = useMemo<string | null>(() => {
+  const winnerCheck = useMemo<string>(() => {
     return !winner
       ? ''
       : winner === 'Cross'
-      ? '❌ won, please restart.'
-      : winner === 'Circle'
-      ? '⭕ won, please restart.'
-      : 'Tied, please restart.';
+        ? '❌ won, please restart.'
+        : winner === 'Circle'
+          ? '⭕ won, please restart.'
+          : 'Tied, please restart.';
   }, [winner]);
 
   return (
@@ -65,7 +65,10 @@ const App = () => {
         <div>{winnerCheck}</div>
         <div className="item-box">
           {items.map((item) => (
-            <button key={item} className="item" onClick={!winner ? () => handleTick(item) : undefined}>
+            <button
+              key={item}
+              className="item"
+              onClick={!winner ? () => handleTick(item) : undefined}>
               <span>
                 {crossArray.includes(item) ? '❌' : circleArray.includes(item) ? '⭕' : ''}
               </span>
